89885c16dc243164c589f7372f8eb8c832f33358
Alexander Mussell <alexander.mussell@hotmail.co.uk>
2020-03-03 21:39:48 +0000

    start chapter 9

--- part_three_functions_as_objects/chapter_7_decorators_and_closures/decorator_test.py
+++ part_three_functions_as_objects/chapter_7_decorators_and_closures/decorator_test.py
@@ -4,6 +4,7 @@ from decorators import clock
 @clock
 def snooze(seconds):
     time.sleep(seconds)
-
+                                                                                                                                                              
 if __name__ == "__main__":
     snooze(.123)
+ 
